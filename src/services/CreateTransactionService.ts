a49f90c6f434d8c760835b26e65e2777811a6aba
Naira Brito <naira.sistemas@gmail.com>
2020-09-15 23:10:48 -0300

    Desafio fundamentos nodejs

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,18 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +20,29 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+
+      throw Error ('Transaction type is invalid ');
+    }
+
+    const  { total } = this.transactionsRepository.getBalance();
+      if(type == 'outcome' && total < value ) {
+        throw Error ('The transaction exceeds the cash value ');
+      }
+      
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    });
+
+    return transaction;
+    
+    // if(transaction) {
+    //   throw Error ('Transaction exceeds cash value ');
+      
+    // }
   }
 }
 
